9040983956382eb1364e1d8b79981dbdc6224204
WeDontNeedTo <danlomaev@yandex.ru>
2022-01-17 10:41:47 +0300

    fix Upload

--- Sources/GXBaseAPI/Upload.swift
+++ Sources/GXBaseAPI/Upload.swift
@@ -17,37 +17,37 @@ public protocol UploadManager: NSObject {
 
 }
 
-public extension UploadManager {
-    
-    func upload(
-        request: URLRequest,
-        fileURL: URL
-    ) -> AnyPublisher<UploadResponse, Error> {
-        let progress: PassthroughSubject<(id: Int, progress: Double), Never> = .init()
-
-        let subject: PassthroughSubject<UploadResponse, Error> = .init()
-        let task: URLSessionUploadTask = URLSession.shared.uploadTask(
-            with: request,
-            fromFile: fileURL
-        ) { data, response, error in
-            if let error = error {
-                subject.send(completion: .failure(error))
-                return
-            }
-            if (response as? HTTPURLResponse)?.statusCode == 200 {
-                subject.send(.response(data: data))
-                return
-            }
-            subject.send(.response(data: nil))
-        }
-        task.resume()
-        return progress
-            .filter{ $0.id == task.taskIdentifier }
-            .setFailureType(to: Error.self)
-            .map { .progress(percentage: $0.progress) }
-            .merge(with: subject)
-            .eraseToAnyPublisher()
-    }
-}
+//public extension UploadManager {
+//    
+//    func upload(
+//        request: URLRequest,
+//        fileURL: URL
+//    ) -> AnyPublisher<UploadResponse, Error> {
+//        let progress: PassthroughSubject<(id: Int, progress: Double), Never> = .init()
+//
+//        let subject: PassthroughSubject<UploadResponse, Error> = .init()
+//        let task: URLSessionUploadTask = URLSession.shared.uploadTask(
+//            with: request,
+//            fromFile: fileURL
+//        ) { data, response, error in
+//            if let error = error {
+//                subject.send(completion: .failure(error))
+//                return
+//            }
+//            if (response as? HTTPURLResponse)?.statusCode == 200 {
+//                subject.send(.response(data: data))
+//                return
+//            }
+//            subject.send(.response(data: nil))
+//        }
+//        task.resume()
+//        return progress
+//            .filter{ $0.id == task.taskIdentifier }
+//            .setFailureType(to: Error.self)
+//            .map { .progress(percentage: $0.progress) }
+//            .merge(with: subject)
+//            .eraseToAnyPublisher()
+//    }
+//}
 
 
