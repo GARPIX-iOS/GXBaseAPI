2e68901dd5cfdd352916605124cb1d6ecb70846f
WeDontNeedTo <danlomaev@yandex.ru>
2022-01-19 13:10:24 +0300

    add compressionQuality param

--- Sources/GXBaseAPI/Upload/Media.swift
+++ Sources/GXBaseAPI/Upload/Media.swift
@@ -13,12 +13,14 @@ public struct Media {
     let filename: String
     let data: Data
     let mimeType: String
+    let compressionQuality: CGFloat
     
-    public init?(withImage image: UIImage, forKey key: String) {
+    public init?(withImage image: UIImage, forKey key: String, compressionQuality: CGFloat = 0.3) {
         self.key = key
         self.mimeType = "image/jpeg"
         self.filename = "imagefile.jpg"
-        guard let data = image.jpegData(compressionQuality: 0.7) else { return nil }
+        self.compressionQuality = compressionQuality
+        guard let data = image.jpegData(compressionQuality: compressionQuality) else { return nil }
         self.data = data
     }
 }
